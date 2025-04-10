953a5e29fd5ebdb101e597235c515bd73ba8f1db
mstailor <tailorms@rknec.edu>
2025-04-10 11:06:41 +0530

    Initial commit for Render deploy

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -57,6 +57,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -164,3 +165,11 @@ TEMPLATES = [
 # Media files (uploads)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-dev-key')
